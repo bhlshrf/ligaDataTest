3d6f5c4ea6ab54aee07de4df9e041a0f6d16d15d
Bahaa <bhlshrf@gmail.com>
2021-08-06 02:00:49 +0300

    favorite items

--- frontend/src/pages/cases.js
+++ frontend/src/pages/cases.js
@@ -1,14 +1,38 @@
+import { useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import useApi from '../hooks/useApi';
 
+function favoriteItems() {
+    const key = 'likedCountries';
+    const likedCountries = JSON.parse(localStorage.getItem(key)) ?? [];
+
+    return Object.freeze({
+        includes: (id) => likedCountries.includes(id),
+        add: (id) => localStorage.setItem(key, JSON.stringify([...likedCountries, id])),
+        remove: (id) => localStorage.setItem(key, JSON.stringify(likedCountries.filter(x => x != id))),
+    });
+
+}
+
+const storage = favoriteItems();
 
 export default function Cases() {
     let history = useHistory();
     let { id } = useParams();
     const { loading, error, data } = useApi(`/api/countries/${id}/cases`);
 
+    const [liked, setLiked] = useState(storage.includes(id));
+
     return <div>
         <h2>Cases</h2>
+        <button onClick={() => {
+            if (!liked)
+                storage.add(id);
+            else
+                storage.remove(id);
+
+            setLiked(!liked)
+        }}>{liked ? 'liked' : 'not liked'} </button>
         {
             loading
                 ? 'loading'
