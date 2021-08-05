d91e2280d1cf75e945664b77b09ac15d38efe3df
Bahaa <bhlshrf@gmail.com>
2021-08-06 01:38:17 +0300

    simple api logging

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,29 @@
 
 const app = require('express')();
 
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
+
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+const expressLogger = expressPino({ logger });
+
 const db = require('./data/db')();
 const covidRoute = require('./routes/covid')(db);
 const { NotFoundRoute, GeneralExceptions } = require('./routes/utils');
 
 
+app.use(expressLogger);
+
 app.use('/api', covidRoute);
 
 app.get('/api/status', (req, res) => {
+    logger.debug('some debug commets');
+
     res.status(200).send({ status: 'working' })
 });
 
 app.use(NotFoundRoute);
-app.use(GeneralExceptions);
+app.use(GeneralExceptions(logger));
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => logger.info(`Listening on port ${port}`));
--- backend/routes/utils.js
+++ backend/routes/utils.js
@@ -3,10 +3,11 @@ function NotFoundRoute(req, res, next) {
     res.status(404).json({ error: 'route not found' });
 }
 
-function GeneralExceptions(err, req, res, next) {
-    console.error('uncatched error', err);
-    res.status(400).json({ error: err.message });
-
+function GeneralExceptions(logger) {
+    return (err, req, res, next) => {
+        logger.error('uncatched error', err);
+        res.status(400).json({ error: err.message });
+    }
 }
 
 module.exports = {
