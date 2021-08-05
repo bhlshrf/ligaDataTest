2aee6b5854c509371bc0909ec9d28105f8c93666
Bahaa <bhlshrf@gmail.com>
2021-08-05 23:52:19 +0300

    fix pagination

--- frontend/src/pages/countries.js
+++ frontend/src/pages/countries.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
+
 import '../App.css';
 
 import useApi from '../hooks/useApi';
@@ -11,24 +13,11 @@ export default function Countries() {
     const [desc, setDesc] = useState(false);
 
     const [limit, setLimit] = useState(10);
-    const [page, setPage] = useState(1);
+    const [page, setPage] = useState(0);
 
     const regions = useApi('/api/regions');
     const countries = useApi(`/api/countries?region_id=${region}` +
-        `&orderBy=${orderBy}&desc=${desc}&limit=${limit}&page=${page}`);
-
-
-    const nextPageAvailable = () => limit == countries?.data?.length ?? 0;
-    const previousPageAvailable = () => page > 1;
-    const nextPage = () => {
-        if (nextPageAvailable())
-            setPage(page + 1)
-    }
-
-    const previousPage = () => {
-        if (previousPageAvailable())
-            setPage(page - 1)
-    }
+        `&orderBy=${orderBy}&desc=${desc}&limit=${limit}&page=${page + 1}`);
 
     return (
         <div className="App">
@@ -40,8 +29,11 @@ export default function Countries() {
                             ? 'loading'
                             : regions.error
                                 ? (<button onClick={regions.refresh} disabled={regions.loading}>refresh</button>)
-                                : <select onChange={v => setRegion(v.target.value)}>
-                                    <option selected value=''>...</option>
+                                : <select defaultValue='' onChange={v => {
+                                    setRegion(v.target.value);
+                                    setPage(0);
+                                }}>
+                                    <option value=''>...</option>
                                     {regions.data?.map(x => <option key={x.id} value={x.id}>{x.region}</option>)}
                                 </select>
                         }
@@ -49,8 +41,8 @@ export default function Countries() {
                     . .
                     <label>
                         order by
-                        <select onChange={v => setOrderBy(v.target.value)}>
-                            <option selected value=''> nothing </option>
+                        <select defaultValue='' onChange={v => setOrderBy(v.target.value)}>
+                            <option value=''> nothing </option>
                             <option value='death'>death</option>
                             <option value='recovered'>recovered</option>
                             <option value='confirmed'>confirmed</option>
@@ -65,27 +57,52 @@ export default function Countries() {
 
                     }
 
-                    <select onChange={v => setLimit(v.target.value)}>
+                    <select defaultValue='10' onChange={v => {
+                        setLimit(v.target.value);
+
+                        const totalPages = Math.ceil(countries.data?.totalCount / v.target.value) - 1;
+                        if (page > totalPages)
+                            setPage(totalPages);
+                    }}>
                         <option value='5'>5</option>
-                        <option selected value='10'>10</option>
+                        <option value='10'>10</option>
                         <option value='20'>20</option>
                     </select>
 
-                    <button onClick={previousPage} disabled={!previousPageAvailable()}> Previous Page </button>
-                    current page: {page}
-                    <button onClick={nextPage} disabled={!nextPageAvailable()}> Next Page </button>
 
                 </div>
                 <hr />
                 {countries.loading
                     ? 'loading'
                     : countries.error ? 'error'
-                        : countries?.data?.map(x =>
-                            <span key={x.id}>
-                                <Link to={`/cases/${x.id}`}>{x.country}</Link> - {x.death} - {x.recovered} - {x.confirmed}
-                            </span>
-                        )}
+                        : <ul>
+                            {
+                                countries.data?.countries?.map(x =>
+                                    <li key={x.id}>
+                                        <Link to={`/cases/${x.id}`}>{x.country}</Link> - {x.death} - {x.recovered} - {x.confirmed}
+                                    </li>
+                                )}
+                        </ul>
+                }
 
+                {
+                    countries.data?.totalCount && <ReactPaginate
+                        pageCount={countries.data?.totalCount / limit}
+                        pageRangeDisplayed={3}
+                        marginPagesDisplayed={1}
+
+                        containerClassName={'pagination'}
+                        activeClassName={'active'}
+
+                        forcePage={page}
+                        marginPagesDisplayed={1}
+                        pageRangeDisplayed={2}
+                        previousLabel={'previous'}
+                        nextLabel={'next'}
+                        breakLabel={'...'}
+                        onPageChange={({ selected }) => setPage(selected)}
+                    />
+                }
             </header>
         </div >
     );
